7f239b8c2c93009d123e904fcde35e6c71edeb4b
miku39-s <124442211+miku39-s@users.noreply.github.com>
2023-07-15 18:50:53 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,98 @@
 )
 (document);
 
+
+// 文字を左から流す
+function slideAnime(){
+	//====左に動くアニメーションここから===
+		$('.leftAnime').each(function(){ 
+			var elemPos = $(this).offset().top-50;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+				//左から右へ表示するクラスを付与
+				//テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+				$(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+				$(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+			}else{
+				//左から右へ表示するクラスを取り除く
+				$(this).removeClass("slideAnimeLeftRight");
+				$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+				
+			}
+		});
+		
+  //====右に動くアニメーションここから===
+  $('.rightAnime').each(function(){
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    // 右から左へ表示するクラスを付与
+    // テキスト要素を挟む親要素（右側）とテキスト要素を元位置でアニメーションをおこなう
+    $(this).addClass("slideAnimeRightLeft ");
+    // 要素を右枠外に移動しCSS アニメーションで右から元の位置に移動
+    $(this).children(".rightAnimeInner").addClass("slideAnimeLeftRight");
+    // 子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+    }else{
+    // 右から左へ表示するクラスを取り除く
+    $(this).removeClass("slideAnimeRightLeft ");
+    $(this).children(".rightAnimeInner").removeClass("slideAnimeLeftRight");
+    }
+    });  
+}
+	
+$(window).scroll(function (){
+		slideAnime();
+});
+
+  
+// TOPへもどるボタン
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+  if (scroll >= 200){//200pxスクロールしたら
+    $('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
+    $('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
+      $('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
+      $('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
+    }
+  }
+  
+  var wH = window.innerHeight; //画面の高さを取得
+  var footerPos =  $('#footer').offset().top; //footerの位置を取得
+  if(scroll+wH >= (footerPos+10)) {
+    var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
+    $('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
+      $('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
 //アコーディオンをクリックした時の動作
 $('.history-index').on('click', function() {//タイトル要素をクリックしたら
 	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
