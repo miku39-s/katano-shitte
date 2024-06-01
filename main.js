fc1eaa7906fefc467cb02a746bb035952d0cb5de
miku39-s <124442211+miku39-s@users.noreply.github.com>
2024-06-01 23:28:00 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,8 +10,52 @@
 )
 (document);
 
+// TOPへもどるボタン
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
 
-// 文字を左から流す
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+  if (scroll >= 200){//200pxスクロールしたら
+    $('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
+    $('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
+      $('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
+      $('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
+    }
+  }
+  
+  var wH = window.innerHeight; //画面の高さを取得
+  var footerPos =  $('#footer').offset().top; //footerの位置を取得
+  if(scroll+wH >= (footerPos+10)) {
+    var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
+    $('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
+      $('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
+    }
+  }
+}
+
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
+// 文字を流す
 function slideAnime(){
 	//====左に動くアニメーションここから===
 		$('.leftAnime').each(function(){ 
@@ -55,64 +99,43 @@ $(window).scroll(function (){
 		slideAnime();
 });
 
+  // ====下から上に動くアニメーションここから===
+  // 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+  　　var elemPos = $(this).offset().top-50; //要素より、50px上の
+  　　var scroll = $(window).scrollTop();
+  　　var windowHeight = $(window).height();
+  　　if (scroll >= elemPos - windowHeight){
+  　　$(this).addClass('fadeUp');
+  　　// 画面内に入ったらfadeUpというクラス名を追記
+  　　}else{
+  　　　$(this).removeClass('fadeUp');
+  　　// 画面外に出たらfadeUpというクラス名を外す
+  　　}
+  　　});
+  }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
   
-// TOPへもどるボタン
-//スクロールした際の動きを関数でまとめる
-function PageTopAnime() {
-
-  var scroll = $(window).scrollTop(); //スクロール値を取得
-  if (scroll >= 200){//200pxスクロールしたら
-    $('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
-    $('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
-  }else{//それ以外は
-    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
-      $('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
-      $('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
-    }
-  }
-  
-  var wH = window.innerHeight; //画面の高さを取得
-  var footerPos =  $('#footer').offset().top; //footerの位置を取得
-  if(scroll+wH >= (footerPos+10)) {
-    var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
-    $('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
-  }else{//それ以外は
-    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
-      $('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
-    }
-  }
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
-
-// #page-topをクリックした際の設定
-$('#page-top').click(function () {
-  $('body,html').animate({
-      scrollTop: 0//ページトップまでスクロール
-  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-  return false;//リンク自体の無効化
-});
-
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 //アコーディオンをクリックした時の動作
 $('.history-index').on('click', function() {//タイトル要素をクリックしたら
-	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
-	$(findElm).slideToggle();//アコーディオンの上下動作
-    
-	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-		$(this).removeClass('close');//クラス名を除去し
-	}else{//それ以外は
-		$(this).addClass('close');//クラス名closeを付与
-	}
+  $('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
 
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去    
+	}else{//それ以外は
+		$('.close').removeClass('close'); //クラス名closeを全て除去した後
+		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+		$(findElm).slideDown(500);//アコーディオンを開く
+	}
 });
 
 // クリッカブルマップ
@@ -127,4 +150,15 @@ $(".iframe-open").modaal({
   after_close:function(){// モーダルが閉じた後に行う動作
     $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
 }
+});
+
+// ハンバーガーメニュー
+$(".openbtn").click(function () {
+  $(this).toggleClass('active');
+  $("#nav-sp").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#nav-sp a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#nav-sp").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
